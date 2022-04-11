7b66aa078cd077276699755cef1c80c4729ae92e
Alex Gilvarry <alex.gilvarry@gmail.com>
2022-04-11 14:28:47 -0600

    knight travails

--- lib/knight_travails.rb
+++ lib/knight_travails.rb
@@ -3,23 +3,40 @@
 # knight travails class
 class KnightsTravails
   def initialize
-    @possible_moves = [[2, 1]] # , [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, -2], [1, -2]].freeze
-    @best_moves = []
+    @possible_moves = [[1, 2], [2, 1], [2, -1], [1, -2],
+                       [-1, -2], [-2, -1], [-2, 1], [-1, 2]]
+    @visited = []
+    @queue = []
   end
 
-  def knight_moves(start, stop, moves = [start])
-    if start == stop && moves.length < @best_moves.length
-      @best_moves = moves 
-        return
-    end
-    @possible_moves.each do |move|
-      new_loc = make_move(start, move)
-      next if validate_move(new_loc) == false
+  def knight_moves(start, stop)
+    @queue.append([start])
+    @visited.append(start)
+    while @queue.length.positive?
+      q_len = @queue.length
 
-      moves.append(new_loc)
-      knight_moves(new_loc, stop, moves)
+      (0..q_len).each do |_i|
+        current = @queue.shift
+        return current if current[-1] == stop
+
+        populate_q(current)
+      end
     end
-    @best_moves
+  end
+
+  def populate_q(current)
+    this = current[-1]
+    @possible_moves.each do |m|
+      moved = make_move(this, m)
+      add_move(current, moved) if validate_move(moved) == true
+    end
+  end
+
+  def add_move(current, moved)
+    new_list = current.map(&:clone)
+    new_list.append(moved)
+    @visited.append(moved)
+    @queue.append(new_list)
   end
 
   def make_move(start, move)
@@ -29,13 +46,18 @@ class KnightsTravails
   end
 
   def validate_move(move)
-    if move[0].negative? || move[1] > 7 || move[0] > 7 || move[1].negative?
-        return false
-    end
-    
-    true
+    return false if @visited.include?(move)
+    return true if move[0].between?(0, 7) && move[1].between?(0, 7)
+
+    false
   end
 end
 
 knight = KnightsTravails.new
-p knight.knight_moves([0, 0], [1, 2])
+p knight.knight_moves([7, 7], [7, 3])
+
+knight2 = KnightsTravails.new
+p knight2.knight_moves([0, 0], [3, 3])
+
+knight3 = KnightsTravails.new
+p knight3.knight_moves([3, 3], [0, 0])

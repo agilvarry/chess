6d74f5fb9f836567d1086dab284bd2ce66600db0
Alex Gilvarry <alex.gilvarry@gmail.com>
2022-04-20 14:51:45 -0600

    bishop tests done

--- lib/pieces/bishop.rb
+++ lib/pieces/bishop.rb
@@ -8,11 +8,39 @@ class Bishop < Piece
 
   def initialize(board, location, color)
     super
-    @moveset = [[1, 0], [0, 1], [-1, 0], [0, -1]]
+    @moveset = [[1, 1], [-1, -1], [-1, 1], [1, -1]]
     @icon = iconset(color)
   end
 
   def iconset(color)
     color == :white ? " \u2657 " : " \u265D "
+  end
+
+  def valid_moves(row = @location[0], col = @location[1], potential = [])
+    @moveset.each do |x, y|
+      row += x
+      col += y
+      potential |= expand(row, col, x, y)
+    end
+
+    potential
+  end
+
+  # keep adding the x and y to the row and col til we hit a piece or the end of the board
+  def expand(row, col, x, y, potential = [])
+    while row.negative? == false && row <= 7 && col.negative? == false && col <= 7
+      puts "#{row}, #{col}"
+      potential_move = @board[row][col]
+      break if potential_move.nil? == false && potential_move.color == @color
+
+      if potential_move.nil? == false && potential_move.color != @color
+        potential << [row, col]
+        break
+      end
+      potential << [row, col]
+      row += x
+      col += y
+    end
+    potential
   end
 end
--- spec/bishop_spec.rb
+++ spec/bishop_spec.rb
@@ -6,55 +6,44 @@ require_relative '../lib/pieces/bishop'
 
 RSpec.describe Bishop do
   board = [
-    [:brk, nil, nil, nil,:bkg, nil, nil,:brk],
-    [:bpn, nil,:bpn,:bpn,:bqn,:bpn,:bsp, nil],
-    [:bsp,:bkt, nil, nil,:bpn,:bkt,:bpn, nil],
-    [nil,  nil, nil,:wpn,:wkt, nil, nil, nil],
-    [nil, :bpn, nil, nil,:wpn, nil, nil, nil],
-    [nil,  nil,:wkt, nil, nil,:wqn, nil,:bpn],
-    [:wpn,:wpn,:wpn, nil,:wbp,:wpn,:wpn,:wpn],
-    [:wrk, nil,:wbp, nil,:wkg, nil, nil,:wrk]
+    [:brk, nil, nil, nil, :bkg, nil, nil, :brk],
+    [:bpn, nil, :bpn, :bpn, :bqn, :bpn, :bsp, nil],
+    [:bsp, :bkt, nil, nil, :bpn, :bkt, :bpn, nil],
+    [nil, nil, nil, :wpn, :wkt, nil, nil, nil],
+    [nil, :bpn, nil, nil, :wpn, nil, nil, nil],
+    [nil, nil, :wkt, nil, nil, :wqn, nil, :bpn],
+    [:wpn, :wpn, :wpn, nil, :wbp, :wpn, :wpn, :wpn],
+    [:wrk, nil, :wbp, nil, :wkg, nil, nil, :wrk]
   ]
 
-  context " white knight hasn't moved" do
+  context " white bishop hasn't moved" do
     subject(:bsh) { described_class.new(board, [7, 2], :white) }
 
-    xit 'bishop is on board' do
+    it 'bishop is on board' do
       board[7][2] = bsh
       board[6][1] = Pawn.new(board, [6, 1], :white)
-      expect(board[7][1].instance_of?(Knight)).to be true
+      expect(board[7][2].instance_of?(Bishop)).to be true
     end
 
-    xit "knight pawn hasn't moved yet" do
-      expect(bsh.valid_moves.sort).to eq [[6,3],[5,4], [4,5], [3,6], [2,7]].sort
+    it "bishop hasn't moved yet" do
+      expect(bsh.valid_moves.sort).to eq [[6, 3], [5, 4], [4, 5], [3, 6], [2, 7]].sort
     end
   end
 
-  board = [
-    [:brk, nil, nil, nil,:bkg, nil, nil,:brk],
-    [:bpn, nil,:bpn,:bpn,:bqn,:bpn,:bsp, nil],
-    [:bsp,:bkt, nil, nil,:bpn,:bkt,:bpn, nil],
-    [nil,  nil, nil,:wpn,:wkt, nil, nil, nil],
-    [nil, :bpn, nil, nil,:wpn, nil, nil, nil],
-    [nil,  nil,:wkt, nil, nil,:wqn, nil,:bpn],
-    [:wpn,:wpn,:wpn, nil,:wbp,:wpn,:wpn,:wpn],
-    [:wrk, nil,:wbp, nil,:wkg, nil, nil,:wrk]
-  ]
-
-  context 'black bishop  has moved' do
-    subject(:bsh2) { described_class.new(board, [3, 0], :black) }
+  context 'black bishop has moved' do
+    subject(:bsh2) { described_class.new(board, [2, 0], :black) }
     before do
       bsh2.instance_variable_set(:@used, true)
     end
 
-    xit 'knight is on board' do
-      board[3][0] = bsh2
-      board[6][4] = Bishop.new(board,[6,4], :white)
-      expect(board[2][1].instance_of?(Knight)).to be true
+    it 'bishop is on board' do
+      board[2][0] = bsh2
+      board[6][4] = Bishop.new(board, [6, 4], :white)
+      expect(board[2][0].instance_of?(Bishop)).to be true
     end
 
-    xit 'black knight moves' do
-      expect(knt2.valid_moves.sort).to eq [[0, 2], [3, 3], [4, 0], [4, 2]].sort
+    it 'black bishop moves' do
+      expect(bsh2.valid_moves.sort).to eq [[1,1],[0,2],[3,1],[4,2], [5,3], [6,4]].sort
     end
   end
 end
